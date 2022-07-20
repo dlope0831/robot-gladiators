7ac99a5f3669949d66118d21ef0d7b053d44ca4d
Daniela Lopez <dlope0831@gmail.com>
2022-07-20 17:37:01 -0500

    console log refreasher

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,10 @@
 var playerName = window.prompt("What is your robot's name?");
 //note the lack of quotation marks around the playerName
 console.log(playerName);
+//this will do math and log 20
+console.log(10 +10);
+//what is this?
+console.log("Our robot's name is " + playerName);
 
 //this creates a funtion named "fight"
 
