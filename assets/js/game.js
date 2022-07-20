93ec7314a48bc5630023b871bc51afa6d6baae76
Daniela Lopez <dlope0831@gmail.com>
2022-07-19 21:59:09 -0500

    added console log to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+//this creates the user data for variable
+var playerName = window.prompt("What is your robot's name?");
+//note the lack of quotation marks around the playerName
+console.log(playerName);
+
+//this creates a funtion named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
+
